af3747a0a1422985633fe554d336267461ec56e5
Elias <eli.lehnert@gmx.de>
2017-10-10 10:13:31 +0200

    Added news directory with files, added activity_news.cml

---
+++ app/src/main/java/org/schulcloud/mobile/data/model/News.java
@@ -0,0 +1,8 @@
+package org.schulcloud.mobile.data.model;
+
+/**
+ * Created by araknor on 10.10.17.
+ */
+
+public class News {
+}
---
+++ app/src/main/java/org/schulcloud/mobile/ui/news/NewsMvpView.java
@@ -0,0 +1,8 @@
+package org.schulcloud.mobile.ui.news;
+
+/**
+ * Created by araknor on 10.10.17.
+ */
+
+public interface NewsMvpView {
+}
